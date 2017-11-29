eaee84e19859ba6e03a593f87eae4966b66685f0
dkittlel1 <dkittle@loyalty.com>
2017-11-29 12:53:27 -0500

    refactoring solution to be more strongly typed

--- src/main/scala/actor/db/DbActor.scala
+++ src/main/scala/actor/db/DbActor.scala
@@ -1,15 +1,34 @@
 package actor.db
 
-import akka.actor.{Actor, ActorLogging}
-import akka.actor.Actor.Receive
+import actor.db.DbActor.{KeyFound, KeyNotFound, RetrieveValue, StoreValue}
+import akka.actor.{Actor, ActorLogging, Props}
 
 import scala.collection.mutable
 
 class DbActor extends Actor with ActorLogging {
-  val db = new mutable.HashMap[String, Object]
+  val db = new mutable.HashMap[String, Any]
+
   override def receive: Receive = {
-    case (k: String, v: Object) => db.put(k, v)
+    case StoreValue(key, value) =>
+      db.put(key, value)
+    case RetrieveValue(key) =>
+      db.get(key) match {
+        case None => sender() ! KeyNotFound
+        case Some(x) => sender() ! KeyFound(x)
+      }
     case o => log.info("unknown message ", o)
   }
 
+}
+
+object DbActor {
+  val props: Props = Props[DbActor]
+
+  sealed trait DbCommand
+  case class RetrieveValue(key: String) extends DbCommand
+  case class StoreValue(key: String, value: Any) extends DbCommand
+
+  sealed trait DbEvent
+  case object KeyNotFound extends DbEvent
+  case class KeyFound(value: Any) extends DbEvent
 }
--- src/test/scala/actor/db/DbActorSpec.scala
+++ src/test/scala/actor/db/DbActorSpec.scala
@@ -1,14 +1,20 @@
 package actor.db
 
-import akka.actor.{ActorSystem, Props}
-import akka.testkit.{TestActorRef, TestKit}
-import org.scalatest.{BeforeAndAfterAll, FlatSpec, FlatSpecLike, Matchers}
+import actor.db.DbActor.{KeyFound, KeyNotFound, RetrieveValue, StoreValue}
+import akka.actor.ActorSystem
+import akka.testkit.{ImplicitSender, TestActorRef, TestKit}
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+
+import scala.concurrent.duration._
 
 class DbActorSpec()
     extends TestKit(ActorSystem("test-system"))
     with FlatSpecLike
     with Matchers
-    with BeforeAndAfterAll {
+    with BeforeAndAfterAll
+    with ImplicitSender {
+
+  implicit val timeout = 2.seconds
 
   override def afterAll(): Unit = {
     super.afterAll()
@@ -19,15 +25,23 @@ class DbActorSpec()
     val actor = TestActorRef(new DbActor)
     val db = actor.underlyingActor
 
-    actor ! ("a", "testing")
+    actor ! StoreValue("a", "testing")
     assert(db.db.contains("a"))
   }
 
-  it should "not store a value if the key is not a string" in {
+  it should "not return a value if no value has been stored" in {
+    val actor = TestActorRef(new DbActor)
+    actor ! RetrieveValue("a")
+    expectMsg(timeout, KeyNotFound)
+  }
+
+  it should "return a value if it's been stored" in {
     val actor = TestActorRef(new DbActor)
     val db = actor.underlyingActor
+    actor ! StoreValue("a", "testing")
+    assert(db.db.contains("a"))
 
-    actor ! (1, "testing")
-    assert(db.db.size === 0)
+    actor ! RetrieveValue("a")
+    expectMsg(timeout, KeyFound("testing"))
   }
 }
